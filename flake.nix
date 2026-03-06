d2f3fe24be19506079fef146bd90f4edbfdbc92e
Josh Heyse <josh@heyse.us>
2026-03-05 22:32:34 -0600

    fix Nix build, upgrade msgpack-cxx, add nix build to CI
    - Guard CPM.cmake download with existence check for Nix sandbox
    - Add all CPM deps as flake inputs with pre-fetched sources
    - Upgrade msgpack-cxx to cpp-7.0.0 with MSGPACK_NO_BOOST
    - Remove boost dependency
    - Add nix build step to CI with fetch-depth: 0 for git tags

--- flake.nix
+++ flake.nix
@@ -4,12 +4,39 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+
+    # CPM dependencies (pinned to match CMakeLists.txt)
+    doctest = {
+      url = "github:doctest/doctest/v2.4.12";
+      flake = false;
+    };
+    tomlplusplus = {
+      url = "github:marzer/tomlplusplus/v3.4.0";
+      flake = false;
+    };
+    cli11 = {
+      url = "github:CLIUtils/CLI11/v2.4.2";
+      flake = false;
+    };
+    spdlog = {
+      url = "github:gabime/spdlog/v1.15.0";
+      flake = false;
+    };
+    msgpack-cxx = {
+      url = "github:msgpack/msgpack-c/cpp-7.0.0";
+      flake = false;
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
+    doctest,
+    tomlplusplus,
+    cli11,
+    spdlog,
+    msgpack-cxx,
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
@@ -18,6 +45,11 @@
         };
         isLinux = pkgs.stdenv.isLinux;
 
+        cpm-cmake = pkgs.fetchurl {
+          url = "https://github.com/cpm-cmake/CPM.cmake/releases/download/v0.42.0/CPM.cmake";
+          sha256 = "12nf9nz1ywy53kqq39c4yk9c7krfd0p381iyk0bli96v8byb8810";
+        };
+
         # Wrap clangd with --query-driver so it discovers Nix compiler system includes
         wrapped-clangd = pkgs.writeShellScriptBin "clangd" ''
           exec ${pkgs.clang-tools}/bin/clangd --query-driver="/nix/store/*/bin/*" "$@"
@@ -25,10 +57,25 @@
       in {
         packages.default = pkgs.clangStdenv.mkDerivation {
           pname = "mupager";
-          version = "0.1.0";
+          version = "0.0.0";
           src = pkgs.lib.cleanSource ./.;
           nativeBuildInputs = with pkgs; [cmake ninja pkg-config];
           buildInputs = with pkgs; [mupdf ncurses];
+
+          preConfigure = ''
+            export CPM_SOURCE_CACHE=$TMPDIR/cpm-cache
+            mkdir -p $CPM_SOURCE_CACHE/cpm
+            cp ${cpm-cmake} $CPM_SOURCE_CACHE/cpm/CPM_0.42.0.cmake
+          '';
+
+          cmakeFlags = [
+            "-DCPM_doctest_SOURCE=${doctest}"
+            "-DCPM_tomlplusplus_SOURCE=${tomlplusplus}"
+            "-DCPM_CLI11_SOURCE=${cli11}"
+            "-DCPM_spdlog_SOURCE=${spdlog}"
+            "-DCPM_msgpack-cxx_SOURCE=${msgpack-cxx}"
+          ];
+
           doCheck = true;
           checkPhase = ''ctest -j$NIX_BUILD_CORES --output-on-failure'';
           meta = with pkgs.lib; {
@@ -73,7 +120,6 @@
 
               # Libraries
               mupdf
-              boost
               ncurses
             ]
             ++ lib.optionals isLinux [
